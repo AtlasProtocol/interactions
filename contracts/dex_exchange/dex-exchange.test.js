e4b03535795698946203d1912835779bece55354
VinciXie <szqq602@163.com>
2019-07-16 19:53:19 +0800

    合约添加测试代码

---
+++ contracts/dex_exchange/dex-exchange.test.js
@@ -0,0 +1,16 @@
+/**
+ * Created by yiren on 2019-07-10
+ *
+ */
+
+"use strict";
+require("nebulas/lib/nvm/native");
+const Exchange = require("./dex-exchange");
+
+const exchange = new Exchange();
+const initAddr = "aaa";
+exchange.init(initAddr);
+
+test("adds 1 + 2 to equal 3", () => {
+  expect(exchange.owner).toBe(initAddr);
+});
