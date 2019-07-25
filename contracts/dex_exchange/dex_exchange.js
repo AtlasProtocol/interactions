a816439b1ef014939ec0b73ca7e8ceded41e05ee
Ren.Yi <ren.yi@atlasp.io>
2019-07-25 17:20:59 +0800

    add EVENT add batch exchange add phase

--- contracts/dex_exchange/dex_exchange.js
+++ contracts/dex_exchange/dex_exchange.js
@@ -1,4 +1,3 @@
-
 "use strict";
 
 /*
@@ -25,106 +24,201 @@
 const STATUS_INITAL = 0; //Attend
 const STATUS_EXCHANGED = 1; //Token exchanged to BEP2
 const STATUS_RETURN = 2; //Fail
+const STATUS_IDLE = 3; //IDLE info submitted
 
 
 class Exchange {
-  constructor() {
-    LocalContractStorage.defineProperty(this, "addressArray"); //array to save address pairs
-    LocalContractStorage.defineProperty(this, "owner");  //admin
-    LocalContractStorage.defineProperty(this, "switch"); //switch of this contract
-    LocalContractStorage.defineProperty(this, "account"); //ATP pledge account
-  }
-
-  init(address, account) {
-    this.addressArray = [];
-    this.owner = address;
-    this.switch = true;
-    this.account = account;
-  }
-
-  submitInfo(hash, bnbAddress) {
-    if (this.switch) {
-      let data = {
-        status: STATUS_INITAL,
-        txHash: hash,
-        bnbAddress: bnbAddress,
-        hash: Blockchain.transaction.hash
-      };
-      let info = this.addressArray;
-      info.push(data);
-
-      this.addressArray = info;
-    } else {
-      return "contract inactivated"
+    constructor() {
+        LocalContractStorage.defineProperty(this, "addressBook"); // save address pairs
+        LocalContractStorage.defineProperty(this, "addressBookExchanged"); // save address pairs that exchanged
+        LocalContractStorage.defineProperty(this, "owner");  //admin
+        LocalContractStorage.defineProperty(this, "valid"); //status of this contract interactions when valid = true
+        LocalContractStorage.defineProperty(this, "pledgeAccount"); //ATP pledge account
+        LocalContractStorage.defineProperty(this, "phase"); //Phase number of exchange offer
     }
-  }
 
-  //Change STATUS_EXCHANGED
-  exchangeCoin(hash) {
-    if (this._accessControl()) {
-      let info = this.addressArray;
-      var index;
-      for (index in info) {
-        if (info[index].hash == hash) {
-          info[index].status = STATUS_EXCHANGED;
-          this.addressArray = info;
-          return info[index];
+    init(address, account) {
+        this.addressBook = {};
+        this.addressBookExchanged = {};
+        this.owner = address;
+        this.valid = true;
+        this.pledgeAccount = account;
+        this.phase = 1;
+    }
+
+    submitInfo(hash, bnbAddress) {
+        if (this.valid) {
+            let data = {
+                status: STATUS_INITAL,
+                txHash: hash,
+                bnbAddress: bnbAddress,
+                hash: Blockchain.transaction.hash,
+                phase: this.phase
+            };
+            let info = this.addressBook;
+            let key = hash + Blockchain.transaction.from;
+            if (info[key]) {
+                return 'Pair existed'
+            } else {
+                info[key] = data;
+                this.addressBook = info;
+                this._submitEvent(data);
+                return 'Info submitted'
+            }
+
+        } else {
+            return "contract inactivated"
         }
-      }
-    } else {
-      return "NO ACCESS";
     }
-  }
 
-  //STATUS_RETURN
-  returnCoin(hash) {
-    if (this._accessControl()) {
-      let info = this.addressArray;
-      var index;
-      for (index in info) {
-        if (info[index].hash == hash) {
-          info[index].status = STATUS_RETURN;
-          this.addressArray = info;
-          return info[index];
+    //Change STATUS_EXCHANGED
+    exchangeCoin(hash, from) {
+        let exchangedInfo = this.addressBookExchanged;
+        if (this._accessControl()) {
+            let info = this.addressBook;
+            let key = hash + from;
+            if (info[key]) {
+                let pair = info[key];
+                pair.status = STATUS_EXCHANGED;
+                exchangedInfo[key] = pair;
+                this.addressBookExchanged = exchangedInfo;
+
+                delete info[key];
+                this.addressBook = info;
+                this._exchangeEvent(pair);
+                return pair;
+            } else {
+                return "No pair found"
+            }
+
+        } else {
+            return "NO ACCESS";
         }
-      }
-    } else {
-      return "NO ACCESS"
     }
-  }
 
-  getInfoByAddress(bnbAddress) {
-    let info = this.addressArray;
-    let index;
-    let result = [];
-    for (index in info) {
-      if (info[index].bnbAddress === bnbAddress) {
-        result.push(info[index])
-      }
+    //batching
+    batchExchangeCoin(keys) {
+        if (!this._accessControl()) {
+            return 'NO ACCESS'
+        }
+        let keyArray = JSON.parse(keys);
+        let exchangedInfo = this.addressBookExchanged;
+        let info = this.addressBook;
+        for (var index in keyArray) {
+            let key = keyArray[index];
+            if (info[key]) {
+                let pair = info[key];
+                pair.status = STATUS_EXCHANGED;
+                exchangedInfo[key] = pair;
+                delete info[key];
+
+            } else {
+              return 'Wrong key'
+            }
+        }
+        this.addressBookExchanged = exchangedInfo;
+        this.addressBook = info;
+        return this.addressBookExchanged;
+
     }
-    return result;
-  }
 
-  getAccountAddress() {
-    return this.account;
-  }
+    //STATUS_RETURN
+    returnCoin(hash, from) {
+        if (this._accessControl()) {
+            let info = this.addressBook;
+            let key = hash + from;
+            if (info[key]) {
+                let pair = info[key];
+                pair.status = STATUS_RETURN;
+                exchangedInfo[key] = pair;
+                this.addressBookExchanged = exchangedInfo;
 
-  changeSwitchStatus() {
-    if (this._accessControl()) {
-      this.switch = !this.switch;
-    }else {
-      return 'NO ACCESS'
+                delete info[key];
+                this.addressBook = info;
+                this._returnEvent(pair);
+                return pair;
+            } else {
+                return "No pair found"
+            }
+        } else {
+            return "NO ACCESS"
+        }
     }
-  }
 
-  _accessControl() {
-    return Blockchain.transaction.from === this.owner;
-  }
+    setPhase(num) {
+        if (!this._accessControl()) {
+            return 'NO ACCESS'
+        }
 
-  dumpAll() {
-    console.log(this.addressArray);
-    return this.addressArray;
-  }
+        this.phase = num;
+        return this.phase;
+    }
+
+    getInfoByAddress(hash, from) {
+        let info = this.addressBook;
+        let exchangedInfo = this.addressBookExchanged;
+        let key = hash + from;
+        if (info[key]) {
+            return info[key]
+        } else if (exchangedInfo[key]) {
+            return exchangedInfo[key];
+        } else {
+            return 'No pair found'
+        }
+    }
+
+    getPhaseInfo() {
+        let phaseInfo = {
+            phase: this.phase,
+            account: this.pledgeAccount,
+        }
+    }
+
+    getAccountAddress() {
+        return this.pledgeAccount;
+    }
+
+    changevalidStatus() {
+        if (this._accessControl()) {
+            this.valid = !this.valid;
+        } else {
+            return 'NO ACCESS'
+        }
+    }
+
+    _accessControl() {
+        return Blockchain.transaction.from === this.owner;
+    }
+
+    _submitEvent(value) {
+       Event.Trigger('Submit',{
+           data:value
+       })
+    }
+
+    _exchangeEvent(value) {
+        Event.Trigger('Exchange',{
+            data:value
+        })
+    }
+
+    _returnEvent(value) {
+        Event.Trigger('Return',{
+            data:value
+        })
+    }
+
+    dumpAddressPair() {
+        return this.addressBook;
+    }
+
+    dumpAddressPairExchanged() {
+        return this.addressBookExchanged;
+    }
+
+    arraytest() {
+        return ['11111','22222'];
+    }
 }
 
 module.exports = Exchange;
