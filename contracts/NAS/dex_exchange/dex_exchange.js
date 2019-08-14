0a81a120e45e8a2aae85353c224715da153870f0
Ren.Yi <ren.yi@atlasp.io>
2019-08-14 15:06:32 +0800

    format name fix

--- contracts/NAS/dex_exchange/dex_exchange.js
+++ contracts/NAS/dex_exchange/dex_exchange.js
@@ -20,18 +20,16 @@
 
 */
 
-
 const STATUS_INITAL = 0; //Attend
 const STATUS_EXCHANGED = 1; //Token exchanged to BEP2
 const STATUS_RETURN = 2; //Fail
 const STATUS_IDLE = 3; //IDLE info submitted
 
-
 class Exchange {
     constructor() {
         LocalContractStorage.defineProperty(this, "addressBook"); // save address pairs
         LocalContractStorage.defineProperty(this, "addressBookExchanged"); // save address pairs that exchanged
-        LocalContractStorage.defineProperty(this, "owner");  //admin
+        LocalContractStorage.defineProperty(this, "owner"); //admin
         LocalContractStorage.defineProperty(this, "valid"); //status of this contract interactions when valid = true
         LocalContractStorage.defineProperty(this, "pledgeAccount"); //ATP pledge account
         LocalContractStorage.defineProperty(this, "phase"); //Phase number of exchange offer
@@ -55,21 +53,21 @@ class Exchange {
                 txHash: hash,
                 bnbAddress: bnbAddress,
                 hash: Blockchain.transaction.hash,
-                phase: this.phase
+                phase: this.phase,
+                from: Blockchain.transaction.from
             };
             let info = this.addressBook;
             let key = hash + Blockchain.transaction.from;
             if (info[key]) {
-                return 'Pair existed'
+                return "Pair existed";
             } else {
                 info[key] = data;
                 this.addressBook = info;
                 this._submitEvent(data);
-                return 'Info submitted'
+                return "Info submitted";
             }
-
         } else {
-            return "contract inactivated"
+            return "contract inactivated";
         }
     }
 
@@ -90,9 +88,8 @@ class Exchange {
                 this._exchangeEvent(pair);
                 return pair;
             } else {
-                return "No pair found"
+                return "No pair found";
             }
-
         } else {
             return "NO ACCESS";
         }
@@ -101,7 +98,7 @@ class Exchange {
     //batching
     batchExchangeToken(keys) {
         if (!this._accessControl()) {
-            return 'NO ACCESS'
+            return "NO ACCESS";
         }
         let keyArray = JSON.parse(keys);
         let exchangedInfo = this.addressBookExchanged;
@@ -113,15 +110,13 @@ class Exchange {
                 pair.status = STATUS_EXCHANGED;
                 exchangedInfo[key] = pair;
                 delete info[key];
-
             } else {
-                return 'Wrong key'
+                return "Wrong key";
             }
         }
         this.addressBookExchanged = exchangedInfo;
         this.addressBook = info;
         return this.addressBookExchanged;
-
     }
 
     //STATUS_RETURN
@@ -141,10 +136,10 @@ class Exchange {
                 this._returnEvent(pair);
                 return pair;
             } else {
-                return "No pair found"
+                return "No pair found";
             }
         } else {
-            return "NO ACCESS"
+            return "NO ACCESS";
         }
     }
 
@@ -165,16 +160,16 @@ class Exchange {
                 this._returnEvent(pair);
                 return pair;
             } else {
-                return "No pair found"
+                return "No pair found";
             }
         } else {
-            return "NO ACCESS"
+            return "NO ACCESS";
         }
     }
 
     setPhase(num) {
         if (!this._accessControl()) {
-            return 'NO ACCESS'
+            return "NO ACCESS";
         }
 
         this.phase = num;
@@ -186,11 +181,11 @@ class Exchange {
         let exchangedInfo = this.addressBookExchanged;
         let key = hash + from;
         if (info[key]) {
-            return info[key]
+            return info[key];
         } else if (exchangedInfo[key]) {
             return exchangedInfo[key];
         } else {
-            return 'No pair found'
+            return "No pair found";
         }
     }
 
@@ -200,7 +195,7 @@ class Exchange {
             account: this.pledgeAccount,
             totalAmount: this.totalAmount,
             valid: this.valid
-        }
+        };
 
         return phaseInfo;
     }
@@ -209,11 +204,11 @@ class Exchange {
         return this.pledgeAccount;
     }
 
-    changevalidStatus() {
+    changeValidStatus() {
         if (this._accessControl()) {
             this.valid = !this.valid;
         } else {
-            return 'NO ACCESS'
+            return "NO ACCESS";
         }
     }
 
@@ -222,21 +217,21 @@ class Exchange {
     }
 
     _submitEvent(value) {
-        Event.Trigger('Submit', {
+        Event.Trigger("Submit", {
             data: value
-        })
+        });
     }
 
     _exchangeEvent(value) {
-        Event.Trigger('Exchange', {
+        Event.Trigger("Exchange", {
             data: value
-        })
+        });
     }
 
     _returnEvent(value) {
-        Event.Trigger('Return', {
+        Event.Trigger("Return", {
             data: value
-        })
+        });
     }
 
     dumpAddressPair() {
